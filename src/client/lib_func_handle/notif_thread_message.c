13793a0b4997459b64514c5f6b1530cf766ad55a
Thomas Rancoule <thomas2.rancoule@epitech.eu>
2020-05-30 23:12:31 +0200

    [ADD] notif thread message

---
+++ src/client/lib_func_handle/notif_thread_message.c
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2020
+** NWP_myteams_2019
+** File description:
+** notif_thread_message
+*/
+
+#include "socket_handle.h"
+
+int client_event_thread_message_received_handle(char *buffer)
+{
+    char *teamuuid = var_parser(buffer, "teamuuid:");
+    char *threaduuid = var_parser(buffer, "threaduuid:");
+    char *useruuid = var_parser(buffer, "useruuid:");
+    char *message = var_parser_body(buffer, "body:");
+
+    if (client_event_thread_message_received(teamuuid, threaduuid, useruuid, message) == -1) {
+        printf("Error in client_event_thread_message_received\n");
+    }
+
+    if (teamuuid != NULL) {
+        free(teamuuid);
+    }
+    if (threaduuid != NULL) {
+        free(threaduuid);
+    }
+    if (useruuid != NULL) {
+        free(useruuid);
+    }
+    if (message != NULL) {
+        free(message);
+    }
+
+    return 0;
+}
