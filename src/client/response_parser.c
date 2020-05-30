0066d20c77686bf516c077cbd0c55ca9ea447609
Thomas Rancoule <thomas2.rancoule@epitech.eu>
2020-05-30 23:34:18 +0200

    [ADD] Switch-case to response_paraser.c

--- src/client/response_parser.c
+++ src/client/response_parser.c
@@ -29,39 +29,47 @@ int err_parsing(int code, char *buffer)
 
 int rsp_parsing(int code, char *buffer)
 {
-    if (code == RSP_USER)
-        client_print_users_handle(buffer);
-    else if (code == RSP_SUBSCRIBE)
-        client_print_subscribed_handle(buffer);
-    else if (code == RSP_UNSUBSCRIBE)
-        client_print_unsubscribed_handle(buffer);
-    else if (code == RSP_CREATE_COMMENT)
-        client_print_reply_created_handle(buffer);
-    else
+    switch (code) {
+        case RSP_USER:
+        return client_print_users_handle(buffer);
+        case RSP_SUBSCRIBE:
+        return client_print_subscribed_handle(buffer);
+        case RSP_UNSUBSCRIBE:
+        return client_print_unsubscribed_handle(buffer);
+        case RSP_CREATE_COMMENT:
+        return client_print_reply_created_handle(buffer);
+        default:
         return 1;
-    return 0;
+    }
 }
 
 int notif_parsing(int code, char *buffer)
 {
-    if (code == NOTIF_MSGRCV)
-        client_event_private_message_received_handle(buffer);
-    else if (code == NOTIF_TEAMCREATE)
-        client_event_team_created_handle(buffer);
-    else
+    switch (code) {
+        case NOTIF_MSGRCV:
+        return client_event_private_message_received_handle(buffer);
+        case NOTIF_TEAMCREATE:
+        return client_event_team_created_handle(buffer);
+        case NOTIF_THREADMSG:
+        return client_event_thread_message_received_handle(buffer);
+        default:
         return 1;
-    return 0;
+    }
 }
 
 void resp_parsing(char *buffer, user_info *info)
 {
     int code = get_code(buffer);
-    if (code == RSP_LOGIN)
+    switch (code) {
+        case RSP_LOGIN:
         client_event_loggedin_handle(buffer, info);
-    else if (code == RSP_LOGOUT)
+        break;
+        case RSP_LOGOUT:
         client_event_loggedout_handle(buffer, info);
-    else if (notif_parsing(code, buffer) == 1 &&
-            rsp_parsing(code, buffer) == 1 &&
-            err_parsing(code, buffer) == 1)
-        printf("%s", buffer);
+        break;
+        default:
+        if (notif_parsing(code, buffer) == 1 && rsp_parsing(code, buffer) == 1
+            && err_parsing(code, buffer) == 1)
+            printf("%s", buffer);
+    }
 }
