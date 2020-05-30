d9349f83eaafa1330733151d87ba8d02c9103135
Vincent DOUROUX <vincent.douroux@epitech.eu>
2020-05-30 23:02:25 +0200

    [FIX] team created is now a notif

--- src/client/response_parser.c
+++ src/client/response_parser.c
@@ -20,19 +20,17 @@ int get_code(char *buffer)
     return atoi(tmp2);
 }
 
-void resp_parsing(char *buffer, user_info *info)
+int err_parsing(int code, char *buffer)
 {
-    int code = get_code(buffer);
-    if (code == RSP_LOGIN)
-        client_event_loggedin_handle(buffer, info);
-    else if (code == RSP_LOGOUT)
-        client_event_loggedout_handle(buffer, info);
-    else if (code == NOTIF_MSGRCV)
-        client_event_private_message_received_handle(buffer);
-    else if (code == RSP_USER)
+    (void) code;
+    (void) buffer;
+    return 1;
+}
+
+int rsp_parsing(int code, char *buffer)
+{
+    if (code == RSP_USER)
         client_print_users_handle(buffer);
-    else if (code == RSP_CREATE_TEAM)
-        client_event_team_created_handle(buffer);
     else if (code == RSP_SUBSCRIBE)
         client_print_subscribed_handle(buffer);
     else if (code == RSP_UNSUBSCRIBE)
@@ -40,5 +38,30 @@ void resp_parsing(char *buffer, user_info *info)
     else if (code == RSP_CREATE_COMMENT)
         client_print_reply_created_handle(buffer);
     else
+        return 1;
+    return 0;
+}
+
+int notif_parsing(int code, char *buffer)
+{
+    if (code == NOTIF_MSGRCV)
+        client_event_private_message_received_handle(buffer);
+    else if (code == NOTIF_TEAMCREATE)
+        client_event_team_created_handle(buffer);
+    else
+        return 1;
+    return 0;
+}
+
+void resp_parsing(char *buffer, user_info *info)
+{
+    int code = get_code(buffer);
+    if (code == RSP_LOGIN)
+        client_event_loggedin_handle(buffer, info);
+    else if (code == RSP_LOGOUT)
+        client_event_loggedout_handle(buffer, info);
+    else if (notif_parsing(code, buffer) == 1 &&
+            rsp_parsing(code, buffer) == 1 &&
+            err_parsing(code, buffer) == 1)
         printf("%s", buffer);
 }
