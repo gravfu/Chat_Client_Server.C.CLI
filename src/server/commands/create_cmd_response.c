662aa897ee1c5199e01cacc31068d6209a8f5c72
Chase Lawrence <chase.lawrence@epitech.eu>
2020-05-31 22:11:53 +0200

    Generic modification

--- src/server/commands/create_cmd_response.c
+++ src/server/commands/create_cmd_response.c
@@ -21,10 +21,10 @@ void create_comment_response(connex_t *user_connex, command_t *cmd,
         "time: \"%s\" body: \"%s\"\r\nEND_RSP\r\n", RSP_CREATE_COMMENT,
         thread->thread_uuid, user_connex->user->user_uuid, timestamp,
         cmd->args[0]);
-    sprintf(notif, "START_RSP\r\n%d\r\nthreaduuid: \"%s\" useruuid: \"%s\" "
-        "time: \"%s\" threadtitle: \"%s\" body: \"%s\"\r\nEND_RSP\r\n",
-        NOTIF_THREADCREATE, thread->thread_uuid, user_connex->user->user_uuid,
-        timestamp, cmd->args[0]);
+    sprintf(notif, "START_RSP\r\n%d\r\nteamuuid: \"%s\" threaduuid: \"%s\" "
+        "useruuid: \"%s\" body: \"%s\"\r\nEND_RSP\r\n",
+        NOTIF_THREADCREATE, thread->p_channel->p_team->team_uuid,
+        thread->thread_uuid, user_connex->user->user_uuid, cmd->args[0]);
     send_direct(user_connex->sock_fd, rsp);
     notify_team(thread->p_channel->p_team, notif, user_connex->user);
 }
@@ -37,7 +37,7 @@ void create_thread_response(const char *uuid_str, connex_t *user_connex)
     thread_t *thread = find_thread(channel->threads, NULL, uuid_str);
 
     sprintf(rsp, "START_RSP\r\n%d\r\nthreaduuid: \"%s\" useruuid: \"%s\" "
-        "time: \"%s\" title: \"%s\" body: \"%s\"\r\nEND_RSP\r\n",
+        "time: \"%s\" title: \"%s\" \"%s\"\r\nEND_RSP\r\n",
         RSP_CREATE_THREAD, uuid_str, user_connex->user->user_uuid,
         thread->timestamp, thread->thread_title, thread->thread_init);
     sprintf(notif, "START_RSP\r\n%d\r\nthreaduuid: \"%s\" useruuid: \"%s\" "
