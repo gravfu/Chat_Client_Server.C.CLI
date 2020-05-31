bc2f5a3a71d977d7256c9c2edcd7da0da412e3fa
Thomas Rancoule <thomas2.rancoule@epitech.eu>
2020-05-31 16:57:44 +0200

    [FIX] rsp list thread now display threads correctly

--- src/client/lib_func_handle/rsp_list_thread.c
+++ src/client/lib_func_handle/rsp_list_thread.c
@@ -7,42 +7,29 @@
 
 #include "socket_handle.h"
 
-void client_channel_print_threads_handle_loop(char *tmp, char *threaduuid,
-    char *useruuid, char *time, char *title, char *body, time_t timestamp)
-{
-    while(threaduuid != NULL) {
-        if (client_channel_print_threads(threaduuid, useruuid, timestamp,
-            title, body) == -1) {
-            printf("Error in client_channel_print_threads\n");
-        }
-        if (threaduuid != NULL) free(threaduuid);
-        threaduuid = NULL;
-        if (useruuid != NULL) free(useruuid);
-        if (time != NULL) free(time);
-        if (title != NULL) free(title);
-        if (body != NULL) free(body);
-        tmp = strstr(tmp, "\n") + 1;
-        threaduuid = var_parser(tmp, "threaduuid:");
-        useruuid = var_parser(tmp, "useruuid:");
-        time = var_parser(tmp, "time:");
-        title = var_parser(tmp, "threadtitle:");
-        body = var_parser_body(tmp, "body:");
-        timestamp = string_to_time_t(time);
-    }
-}
-
 int client_channel_print_threads_handle(char *buffer)
 {
     char *tmp = strstr(buffer, "\n") + 1;
-    char *threaduuid = var_parser(tmp, "threaduuid:");
-    char *useruuid = var_parser(tmp, "useruuid:");
-    char *time = var_parser(tmp, "time:");
+    char *t_uuid = var_parser(tmp, "threaduuid:");
+    char *u_uuid = var_parser(tmp, "useruuid:");
     char *title = var_parser(tmp, "threadtitle:");
     char *body = var_parser_body(tmp, "body:");
-    time_t timestamp = string_to_time_t(time);
+    time_t time = string_to_time_t(var_parser(tmp, "time:"));
 
     tmp = strstr(tmp, "\n") + 1;
-    client_channel_print_threads_handle_loop(tmp, threaduuid, useruuid, time,
-        title, body, timestamp);
+    while(t_uuid != NULL) {
+        if (client_channel_print_threads(t_uuid, u_uuid, time, title, body) == -1)
+            printf("Error in client_team_print_channels\n");
+        if (t_uuid != NULL) free(t_uuid);
+        if (u_uuid != NULL) free(u_uuid);
+        if (title != NULL) free(title);
+        if (body != NULL) free(body);
+        tmp = strstr(tmp, "\n") + 1;
+        t_uuid = var_parser(tmp, "threaduuid:");
+        u_uuid = var_parser(tmp, "useruuid:");
+        title = var_parser(tmp, "threadtitle:");
+        body = var_parser_body(tmp, "body:");
+        time = string_to_time_t(var_parser(tmp, "time:"));
+    }
     return 0;
 }
